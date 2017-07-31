7ca5288084990740be7468397482450c7d277e84
Josue Rosales <jhosue.rosales@gmail.com>
2017-07-31 09:52:55 -0400

    still working on schema and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  post1 = Post.create([{ content: 'Star Wars' }, { is_published: true }])
+
+  Character.create(name: 'Luke', movie: movies.first)
+
+
+x1 = Post.create({content: "thing1", is_published:  true)
+x2= Post.create({ content: "thimg2", is_published:  true)
+x3= Post.create({ content: "thing3", is_published:  true)
+
+
+y1= Comment.create({:body: "hi1", post: x1})
+y2= Comment.create({ :body: "hi1", post: x2})
+y3= Comment.create({ :body: "hi1", post: x3})
